d84f422e64f42d619915ebe2e5caadad02488aab
caedenph <caedenperelliharris@gmail.com>
2023-08-29 10:40:47 +0100

    chore(tests): Fix failing lint tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,6 +2,9 @@ import unittest
 
 
 class TestExample(unittest.TestCase):
+    """Example test case"""
+
     def test_example(self) -> None:
+        """Example test function"""
         self.assertEqual(1 + 1, 2)
         self.assertGreater(10, 5)
